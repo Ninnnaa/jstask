56da293e84ff3d6dc8e9a0a8d2d77ef1dcf880b1
Nina Apostu <apostuninuta@gmail.com>
2021-03-26 11:34:41 +0200

    get factorial 26

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 function whileFunction() {
     let count = 5;
-    while (count <= 15){
-    console.log(count);
+    while (count <= 15) {
+        console.log(count);
         count += 1;
+    }
 }
+
+function factorialFunction() {
+    let num = 26;
+    let fact = 1;
+    for (let i = 1; i <= num; i++){
+        fact *= i;
+    }
+    console.log(fact.toLocaleString('fullwide', {useGrouping:false}));
 }
